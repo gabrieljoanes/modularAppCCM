ce706b43c12188b6c9844203697c24d5d4ba8dea
gabrieljoanes <128034350+gabrieljoanes@users.noreply.github.com>
2025-05-15 10:19:35 +0300

    Update transition_cleaner.py

--- utils/transition_cleaner.py
+++ utils/transition_cleaner.py
@@ -4,18 +4,28 @@ from nltk.stem.snowball import FrenchStemmer
 stemmer = FrenchStemmer()
 
 def clean_transitions(transitions):
-    seen_stems = set()
+    seen_prefixes = set()
+    autre_used = False
     cleaned = []
 
     for t in transitions:
-        intro = " ".join(t.split()[:4]).lower()
-        intro_clean = re.sub(r'[^\w\s]', '', intro.strip())
-        intro_stemmed = " ".join([stemmer.stem(w) for w in intro_clean.split()])
+        t_lower = t.lower()
+        intro = " ".join(t.split()[:4])
+        intro_clean = re.sub(r'[^\w\s]', '', intro.lower())
 
-        if intro_stemmed in seen_stems:
+        # Rule: block multiple "autre" usage
+        if "autre" in t_lower:
+            if autre_used:
+                cleaned.append("[REPETITION: 'autre' already used]")
+                continue
+            else:
+                autre_used = True
+
+        # Rule: block exact prefix reuse
+        if intro_clean in seen_prefixes:
             cleaned.append("[REPETITION: Replace manually]")
         else:
-            seen_stems.add(intro_stemmed)
+            seen_prefixes.add(intro_clean)
             cleaned.append(t)
 
     return cleaned
